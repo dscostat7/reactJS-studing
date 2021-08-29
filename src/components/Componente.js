0e43d763085ab3b972f369a6450014603282c6a7
Diego <diego.s.costa@fiesc.com.br>
2021-08-28 21:00:43 -0300

    initial commit

---
+++ src/components/Componente.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Componente extends Component {
+    render() {
+        return (
+            <div>
+                I'am a component!
+            </div>
+        );
+    }
+}
+
+export default Componente;
